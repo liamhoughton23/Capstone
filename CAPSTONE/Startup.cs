1c10e006e6b24fa149b5a805ff0c469258ec2dae
Liam Houghton <liamhoughton90@yahoo.com>
2018-02-21 11:51:33 -0600

    first commit

---
+++ CAPSTONE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CAPSTONE.Startup))]
+namespace CAPSTONE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
